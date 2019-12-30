92f578d5ae850dcc2811890094a254e2a91ecc34
Dale Stevens <dale@twilightcoders.net>
2019-12-30 13:18:02 -0700

    Improved performance for 6.0 query methods
    Mirror improvements found in 6.0 master:
    https://github.com/rails/rails/commit/f854b46aba0cdd8c6eb92ef038e1e6a3c3aa17f6
    https://github.com/rails/rails/commit/58df9a452fa75f10365849775292dcf0ae79a6f2

--- lib/active_record/framing/compat/active_record_6_0.rb
+++ lib/active_record/framing/compat/active_record_6_0.rb
@@ -2,14 +2,12 @@ module ActiveRecord
   module Framing
     module QueryMethods
       {
-        frames: ::ActiveRecord::QueryMethods::FROZEN_EMPTY_HASH,
-        reframe: ::ActiveRecord::QueryMethods::FROZEN_EMPTY_HASH
+        frames: 'ActiveRecord::QueryMethods::FROZEN_EMPTY_HASH',
+        reframe: 'ActiveRecord::QueryMethods::FROZEN_EMPTY_HASH'
       }.each do |value_name, default_value|
-        ::ActiveRecord::QueryMethods::DEFAULT_VALUES[value_name] = default_value
         class_eval <<-CODE, __FILE__, __LINE__ + 1
           def #{value_name}_values                                                 # def frames_values
-            default = ::ActiveRecord::QueryMethods::DEFAULT_VALUES[:#{value_name}] #   default = DEFAULT_VALUES[:frames]
-            @values.fetch(:#{value_name}, default)                                 #   @values.fetch(:frames, default)
+            @values.fetch(:#{value_name}, #{default_value})                        #   @values.fetch(:frames, FROZEN_EMPTY_HASH)
           end                                                                      # end
 
           def #{value_name}_values=(value)     # def frames_values=(value)
