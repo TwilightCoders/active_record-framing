565143c031bcd9215c0403706a1104a08781a513
Dale Stevens <dale@twilightcoders.net>
2020-01-03 08:18:54 -0700

    Clean up ModelProxy for readability and understandability
    Removes an unused method (is_a?)
    Rewords an explanatory comment about context
    Uses super keyword for better readability

--- lib/active_record/framing/model_proxy.rb
+++ lib/active_record/framing/model_proxy.rb
@@ -8,14 +8,14 @@ module ActiveRecord
       end
 
       def unscoped
-        __getobj__.unscoped.tap do |rel|
+        super.tap do |rel|
           rel.instance_variable_set(:@table, @table)
         end
       end
 
       # NOTE: Arel 8.x / Rails 5.1 compatibility
-      # Method exists on AR::Model, but default value for table
-      # is scoped incorrectly without this proxy override
+      # Method exists on AR::Model, but default value for table attribute
+      # is gathered in the wrong context without this proxy override
       def arel_attribute(column_name, table = arel_table)
         super
       end
@@ -44,10 +44,6 @@ module ActiveRecord
         @table
       end
 
-      # Might not need this
-      def is_a?(obj)
-        __getobj__.is_a?(obj)
-      end
     end
   end
 end
